23effa0850f79c0f74002cd3e9d95d82234298f1
Sudarshan Bhide <sudarshan.bhide@embold.io>
2020-07-25 13:31:38 +0530

    wait a while for the scan status

--- index.js
+++ index.js
@@ -3,16 +3,53 @@ const github = require('@actions/github');
 
 const axios = require('axios');
 
+async function waitForScanStatus(emboldUrl, token, repoUid, scanId) {
+    try {
+        // Check for upto 1 min if the scan enters processing state
+        let status = null;
+        for (let i = 0; i < 30; ++i) {
+            status = await axios.get(emboldUrl + '/api/v1/repositories/' + repoUid + '/scans/' + scanId + '/status', {
+                headers: { 'Authorization': 'Bearer ' + token, 'Content-Type': 'application/x-www-form-urlencoded' }
+            });
+
+            if (status.data.scanStatus !== 'FAIL') {
+                if (status.data.currentStep === 'SCANBOX_SCHEDULED_SUCCESS') {
+                    // Scan is scheduled, so all good
+                    console.log(`Scan with id ${scanId} is started successfully`);
+                    return;
+                }
+            } else {
+                // Scan failed, so return with failure
+                throw new Error(`Scan with id ${scanId} failed with error: ${status.data.currentStep}`);
+            }
+
+            // else wait some more...
+            await new Promise(r => setTimeout(r, 2000));
+        }
+
+        // If we reached here we still didn't get the expected status. This could be because the scm sync is taking longer (e.g. large repo), so we can still return
+        if (status && status.data.currentStep === 'UPDATING_SOURCES') {
+            console.log(`Scan with id ${scanId} is updating sources`);
+        }
+    } catch (error) {
+        console.error(`Error while checking scan status: ${error.message} for scanId: ${scanId}`)
+        throw error;
+    }
+}
+
 async function launchScan(emboldUrl, token, repoUid, branch) {
 
     console.log(`Launching Embold scan for repo: ${repoUid} at: ${emboldUrl}`);
     try {
 
         let res = await axios.post(emboldUrl + '/api/v1/repositories/' + repoUid + '/scan', `repoBranchOrTag=${branch}`, {
-        headers: { 'Authorization': 'Bearer ' + token, 'Content-Type': 'application/x-www-form-urlencoded' }
-          });
-        console.log(res.data.status);
-    } catch(error) {
+            headers: { 'Authorization': 'Bearer ' + token, 'Content-Type': 'application/x-www-form-urlencoded' }
+        });
+
+        // Check back on scan status with the status API until we get the expected state
+        await waitForScanStatus(emboldUrl, token, repoUid, res.data.scanId);
+    } catch (error) {
+        console.log(error);
         core.setFailed(error.message);
     }
 }
@@ -27,5 +64,5 @@ try {
     core.setOutput("status", "SUCCESS");
 
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
